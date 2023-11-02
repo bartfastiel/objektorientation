c3fd3c08efb3f5521a6499b97d82a8a749cffcbb
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-02 09:31:46 +0100

    Define class "Screw"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,21 +3,29 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        String screw1Type = "torx";
-        int screw1Length = 50;
-        double screw1Width = 4.0;
+        Screw screw1 = new Screw();
+        screw1.type = "torx";
+        screw1.length = 50;
+        screw1.width = 4.0;
 
-        String screw2Type = "torx";
-        int screw2Length = 50;
-        double screw2Width = 5.0;
+        Screw screw2 = new Screw();
+        screw2.type = "torx";
+        screw2.length = 50;
+        screw2.width = 5.0;
 
-        printScrew(screw1Type, screw1Length, screw1Width);
-        printScrew(screw2Type, screw2Length, screw2Width);
+        Screw screw3 = new Screw();
+        screw3.type = "kreuzschlitz";
+        screw3.length = 40;
+        screw3.width = 3.0;
+
+        printScrew(screw1);
+        printScrew(screw2);
+        printScrew(screw3);
 
     }
 
-    public static void printScrew(String type, int length, double width) {
-        System.out.println("Schraube mit Kopf " + type + ", Länge " + length + " Breite " + width);
+    public static void printScrew(Screw screw) {
+        System.out.println("Schraube mit Kopf " + screw.type + ", Länge " + screw.length + " Breite " + screw.width);
     }
 }
 
---
+++ src/main/java/org/example/Screw.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Screw {
+
+    String type;
+    int length;
+    double width;
+
+}
